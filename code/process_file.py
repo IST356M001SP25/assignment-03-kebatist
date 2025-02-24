52b6d92c3dd36df3de496bf07f9b1bf0d8363215
kebatist <kebatist@syr.edu>
2025-02-24 10:57:21 -0500

    process file edits again

--- code/process_file.py
+++ code/process_file.py
@@ -5,13 +5,51 @@ You should output the parsed package and total package size for each package in 
 Screenshot available as process_file.png
 '''
 import streamlit as st 
-from io import StringIO 
 import packaging
+from io import StringIO 
 import json
 
 st.title("process file")
 
+file = st.file_uploader("Upload package file:")
+
+if file:
+    filename=file.name
+    json_filename = filename.replace(".txt",".json")
+    packages = []
+    text = StringIO(file.getvalue().decode("utf-8")).read()
+    for line in text.split("\n"):
+        line = line.strip()
+        pack = packaging.parse_packaging(line)
+        total = packaging.calc_total_units(pack)
+        unit = packaging.get_unit(pack)
+        packages.append(pack)
+        st.info(f"{line} ➡️ Total 📦 Size: {total} {unit}")
+        count = len(packages)
+    with open(f"./data/{json_filename}", "w") as f:
+        json.dump(packages, f, indent=4)
+    st.success(f"{count} packages written to {json_filename}", icon="💾")
+'''
+DRAFT TWO 
 file = st.file_uploader("upload file")
+filename = file.name
+json_filename = filename.replace(".txt",".json")
+packages = []
+text = StringIO(file.getvalue().decode("utf-8")).read()
+for line in text.split("\n"):
+    line = line.strip()
+    pack = packaging.parse_packaging(line)
+    total = packaging.calc_total_units(pack)
+    unit = packaging.get_unit(pack)
+    packages.append(pack)
+    st.info(f"{line} ➡️ Total 📦 Size: {total} {unit}")
+count = len(packages)
+with open(f"./data/{json_filename}", "w") as f:
+    json.dump(packages, f, indent=4)
+st.success(f"{count} packages written to {json_filename}", icon="💾")
+'''
+'''
+DRAFT ONE
 if file:
     file.seek(0)
     file_contents = file.read().decode("utf-8")
@@ -25,3 +63,6 @@ if file:
         size = list(item.values())[0]
         st.info(f"{name} ➡️ {size}")
     st.success(f"Total 📦 Size: {total} {unit}")
+
+    count += 1
+    '''
