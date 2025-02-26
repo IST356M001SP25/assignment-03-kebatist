b0ca685166474015d795d209f1f6f0928259beab
kebatist <kebatist@syr.edu>
2025-02-26 16:14:04 -0500

    remove deprecated code and comments from process_file.py

--- code/process_file.py
+++ code/process_file.py
@@ -1,34 +1,34 @@
 '''
 Next, write a streamlit to read ONE file of packaging information. 
 You should output the parsed package and total package size for each package in the file.
-
+#ur mom
 Screenshot available as process_file.png
 '''
-import streamlit as st 
-import packaging
-from io import StringIO 
-import json
+import streamlit as st
+import packaging 
+from io import StringIO   
+import json 
 
-st.title("process file")
+st.title("process file") 
 
-file = st.file_uploader("Upload package file:")
+file = st.file_uploader("Upload package file:") 
 
-if file:
-    filename=file.name
-    json_filename = filename.replace(".txt",".json")
+if file: 
+    filename=file.name 
+    json_filename = filename.replace(".txt",".json") 
     packages = []
-    text = StringIO(file.getvalue().decode("utf-8")).read()
-    for line in text.split("\n"):
-        line = line.strip()
-        pack = packaging.parse_packaging(line)
-        total = packaging.calc_total_units(pack)
-        unit = packaging.get_unit(pack)
-        packages.append(pack)
-        st.info(f"{line} ➡️ Total 📦 Size: {total} {unit}")
-        count = len(packages)
-    with open(f"./data/{json_filename}", "w") as f:
-        json.dump(packages, f, indent=4)
-    st.success(f"{count} packages written to {json_filename}", icon="💾")
+    text = StringIO(file.getvalue().decode("utf-8")).read() 
+    for line in text.split("\n"): 
+        line = line.strip() 
+        pack = packaging.parse_packaging(line) 
+        total = packaging.calc_total_units(pack) 
+        unit = packaging.get_unit(pack) 
+        packages.append(pack) 
+        st.info(f"{line} ➡️ Total 📦 Size: {total} {unit}") 
+        count = len(packages) 
+    with open(f"./data/{json_filename}", "w") as f: 
+        json.dump(packages, f, indent=4) 
+    st.success(f"{count} packages written to {json_filename} ", icon="💾")
 '''
 DRAFT TWO 
 file = st.file_uploader("upload file")
